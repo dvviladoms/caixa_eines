7b6891eaf9052f5f03ae4c3abe1be898638fd5a3
David Vidal Viladoms <50232764+dvviladoms@users.noreply.github.com>
2026-03-26 12:54:35 +0100

    Add files via upload

---
+++ enllacos.js
@@ -0,0 +1,72 @@
+const dadesEnllacos = [
+  {
+    categoria: "Programari",
+    enllacos: [
+      { 
+        titol: "Diagrams.net", 
+        url: "https://app.diagrams.net/?src=about", 
+        descripcio: "Creació de diagrames en línia." 
+      }
+    ]
+  },
+  {
+    categoria: "Conversors i programació",
+    enllacos: [
+      { 
+        titol: "ILovePDF", 
+        url: "https://www.ilovepdf.com/", 
+        descripcio: "Modificació i edició d'arxius PDF." 
+      },
+      { 
+        titol: "ILoveIMG", 
+        url: "https://www.iloveimg.com/", 
+        descripcio: "Conversió de formats d'arxius d'imatge." 
+      },
+      { 
+        titol: "FreeConvert", 
+        url: "https://www.freeconvert.com/", 
+        descripcio: "Conversió de formats de diversos tipus d'arxius." 
+      },
+      { 
+        titol: "Text Compare", 
+        url: "https://text-compare.com/", 
+        descripcio: "Comparació de text en dues columnes paral·leles." 
+      },
+      { 
+        titol: "Online Comma Separator", 
+        url: "https://capitalizemytitle.com/tools/column-to-comma-separated-list/", 
+        descripcio: "Conversió de columnes de text a llista de comes separades (per a SQL)." 
+      }
+    ]
+  },
+  {
+    categoria: "Validadors de dades",
+    enllacos: [
+      { 
+        titol: "IBAN Calculator", 
+        url: "https://www.ibancalculator.com/", 
+        descripcio: "Validació de comptes IBAN i codis bancaris BIC/SWIFT." 
+      },
+      { 
+        titol: "VIES Validator (European Comission)", 
+        url: "https://ec.europa.eu/taxation_customs/vies/#/vat-validation", 
+        descripcio: "Validació de CIF d'empreses intracomunitàries al sistema VIES." 
+      },
+      { 
+        titol: "Letra NIF", 
+        url: "https://www.letranif.com/", 
+        descripcio: "Validació de números i lletres de NIF/DNI/NIE/CIF." 
+      },
+      { 
+        titol: "Empresia", 
+        url: "https://www.empresia.es/", 
+        descripcio: "Obtenció de dades fiscals i mercantils d'empreses." 
+      },
+      { 
+        titol: "Códigos Postales", 
+        url: "https://www.codigospostales.com/mapfus.html", 
+        descripcio: "Consulta de delimitacions de codis postals." 
+      }
+    ]
+  }
+];
